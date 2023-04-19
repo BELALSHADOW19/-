8a11a97742def76ea91a7f3e18234f588c96ac73
SHADOW ALZAYEEM <116435902+BELALSHADOW19@users.noreply.github.com>
2023-04-19 22:09:14 +0200

    Update h.py

--- h.py
+++ h.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 
-api_id = 
-api_hash = ""
-bot_token = ""
+api_id = "15170853"
+api_hash = "33f17f7ccfa43fd779489dca8ab67e34"
+bot_token = "6291803682:AAHIWpA4xRm9J2XHKA_2xIZGwDHB6PrUtCA"
 
 app = Client(
     "my_bot",
